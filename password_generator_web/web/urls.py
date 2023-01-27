81f68fb5e34690b518caf3a79c58f45ba17ba328
reniboyanova <reni91boyanova@gmail.com>
2023-01-27 10:43:27 +0200

    Make a navigation, do all the views

--- password_generator_web/web/urls.py
+++ password_generator_web/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from password_generator_web.web.views import index_page, generate_unique_pass
+from password_generator_web.web.views import index_page, generate_unique_pass, encrypt_password
 
 urlpatterns = (
     path('', index_page, name='index page'),
     path('generate-password/', generate_unique_pass, name='generate pass'),
+    path('encrypt-password/', encrypt_password, name='encrypt pass'),
 )
--- password_generator_web/web/views.py
+++ password_generator_web/web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import random
 
-from password_generator_web.web.forms import UniquePassForm
+from password_generator_web.web.forms import UniquePassForm, EncryptionPass
 
 
 class PassUniqueGenerator:
@@ -17,26 +17,28 @@ class PassUniqueGenerator:
         self.numbers = numbers
         self.password_list = []
 
-    def letters_choice(self):
+    def choice(self):
         for char in range(1, self.letters + 1):
             self.password_list.append(random.choice(self.LETTERS))
-
-    def symbols_choice(self):
         for char in range(1, self.symbols + 1):
             self.password_list.append(random.choice(self.SYMBOL))
-
-    def number_choice(self):
         for char in range(1, self.numbers + 1):
             self.password_list.append(random.choice(self.NUMBERS))
 
-    def shuffle_pass(self):
-        return random.shuffle(self.password_list)
-
     def generated_pass(self):
+        random.shuffle(self.password_list)
         password_for_print = ""
         for char in self.password_list:
             password_for_print += char
         return password_for_print
+
+
+def encrypting(text, shift):
+    plain_text = ""
+    for ch in text:
+        plain_text += chr(ord(ch) + shift)
+
+    return plain_text
 
 
 # Create your views here.
@@ -49,15 +51,15 @@ def generate_unique_pass(request):
         form = UniquePassForm()
     else:
         form = UniquePassForm(request.POST)
+
         if form.is_valid():
             nr_letters = form.cleaned_data['number_of_letters']
             nr_numbers = form.cleaned_data['number_of_numbers']
             nr_symbols = form.cleaned_data['number_of_symbols']
+
             password = PassUniqueGenerator(nr_letters, nr_symbols, nr_numbers)
-            password.letters_choice()
-            password.number_choice()
-            password.symbols_choice()
-            password.shuffle_pass()
+            password.choice()
+
             password_for_print = password.generated_pass()
             context = {'password': password_for_print}
 
@@ -67,3 +69,24 @@ def generate_unique_pass(request):
 
     }
     return render(request, 'generate_password.html', context)
+
+
+def encrypt_password(request):
+    if request.method == 'GET':
+        form = EncryptionPass()
+    else:
+        form = EncryptionPass(request.POST)
+
+        if form.is_valid():
+            user_password = form.cleaned_data['your_password']
+            key_to_encrypt = form.cleaned_data['encrypter_key']
+
+            password = encrypting(user_password, key_to_encrypt)
+            context = {'password': password}
+
+            return render(request, 'success_encrypted_password.html', context)
+    context = {
+        'form': form,
+
+    }
+    return render(request, 'encrypt_password.html', context)
